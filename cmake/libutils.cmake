07656c66c7cf1b450f809c33f893d1cb30be953c
caseymcc <caseymcc@krazer.net>
2018-11-01 13:15:17 -0500

    fix multi-configurations and ignore gcc warnings

--- cmake/libutils.cmake
+++ cmake/libutils.cmake
@@ -128,39 +128,48 @@ MACRO(MERGE_STATIC_LIBS TARGET OUTPUT_NAME LIBS_TO_MERGE)
   # To produce a library we need at least one source file.
   # It is created by ADD_CUSTOM_COMMAND below and will
   # also help to track dependencies.
+  IF(CMAKE_CONFIGURATION_TYPES)
+    FOREACH(CONFIG_TYPE ${CMAKE_CONFIGURATION_TYPES})
+      STRING(TOUPPER ${CONFIG_TYPE} CONFIG_TYPE)
+      SET(CONFIGURATION_TYPES ${CONFIGURATION_TYPES} ${CONFIG_TYPE})
+    ENDFOREACH()
+  ELSE()
+    STRING(TOUPPER ${CMAKE_BUILD_TYPE} BUILD_TYPE)
+    SET(CONFIGURATION_TYPES ${BUILD_TYPE})
+  ENDIF()
+
   SET(SOURCE_FILE ${CMAKE_CURRENT_BINARY_DIR}/${TARGET}_depends.c)
   ADD_LIBRARY(${TARGET} STATIC ${SOURCE_FILE})
   SET_TARGET_PROPERTIES(${TARGET} PROPERTIES OUTPUT_NAME ${OUTPUT_NAME})
 
-  SET(OSLIBS)
+  # Make the generated dummy source file depended on all static input
+  # libs. If input lib changes,the source file is touched
+  # which causes the desired effect (relink).
+  ADD_CUSTOM_COMMAND( 
+    OUTPUT  ${SOURCE_FILE}
+    COMMAND ${CMAKE_COMMAND}  -E touch ${SOURCE_FILE}
+    DEPENDS ${STATIC_LIBS})
+      
+  FOREACH(CONFIG_TYPE ${CONFIGURATION_TYPES})
+    SET(STATIC_LIBS_${CONFIG_TYPE})
+  ENDFOREACH()
 
+  SET(OSLIBS)
   FOREACH(LIB ${LIBS_TO_MERGE})
-    IF(CMAKE_CONFIGURATION_TYPES)
-      FOREACH(CONFIG_TYPE ${CMAKE_CONFIGURATION_TYPES})
-        GET_TARGET_PROPERTY(LIB_LOCATION_${CONFIG_TYPE} ${LIB} LOCATION_${CONFIG_TYPE})
-      ENDFOREACH()
-    ELSE()
-      GET_TARGET_PROPERTY(LIB_LOCATION ${LIB} LOCATION_${CMAKE_BUILD_TYPE})
-    ENDIF()
-  
     GET_TARGET_PROPERTY(LIB_TYPE ${LIB} TYPE)
+    GET_TARGET_PROPERTY(LIB_LOCATION ${LIB} LOCATION)
     IF(NOT LIB_LOCATION)
-        # 3rd party library like libz.so. Make sure that everything
-        # that links to our library links to this one as well.
-        LIST(APPEND OSLIBS ${LIB})
+      # 3rd party library like libz.so. Make sure that everything
+      # that links to our library links to this one as well.
+      LIST(APPEND OSLIBS ${LIB})
     ELSE()
       # This is a target in current project
       # (can be a static or shared lib)
       IF(LIB_TYPE STREQUAL "STATIC_LIBRARY")
-        IF(CMAKE_CONFIGURATION_TYPES)
-          FOREACH(CONFIG_TYPE ${CMAKE_CONFIGURATION_TYPES})
-            SET(STATIC_LIBS_${CONFIG_TYPE} ${STATIC_LIBS_${CONFIG_TYPE}} ${LIB_LOCATION_${CONFIG_TYPE}})
-          ENDFOREACH()
-        ELSE()
-          SET(STATIC_LIBS ${STATIC_LIBS} ${LIB_LOCATION})
-        ENDIF()
-        
-        SET(STATIC_LIBS_DEBUG ${STATIC_LIBS_DEBUG} ${LIB_LOCATION_DEBUG})
+        FOREACH(CONFIG_TYPE ${CONFIGURATION_TYPES})
+          GET_TARGET_PROPERTY(LIB_LOCATION_${CONFIG_TYPE} ${LIB} LOCATION_${CONFIG_TYPE})
+          SET(STATIC_LIBS_${CONFIG_TYPE} ${STATIC_LIBS_${CONFIG_TYPE}} ${LIB_LOCATION_${CONFIG_TYPE}})
+        ENDFOREACH()
         ADD_DEPENDENCIES(${TARGET} ${LIB})
         # Extract dependend OS libraries
         GET_DEPENDEND_OS_LIBS(${LIB} LIB_OSLIBS)
@@ -171,76 +180,61 @@ MACRO(MERGE_STATIC_LIBS TARGET OUTPUT_NAME LIBS_TO_MERGE)
       ENDIF()
     ENDIF()
   ENDFOREACH()
-  
 
   IF(OSLIBS)
     LIST(REMOVE_DUPLICATES OSLIBS)
     TARGET_LINK_LIBRARIES(${TARGET} ${OSLIBS})
     MESSAGE(STATUS "Library ${TARGET} depends on OSLIBS ${OSLIBS}")
   ENDIF()
-
-  IF(STATIC_LIBS)
-    LIST(REMOVE_DUPLICATES STATIC_LIBS)
-  ENDIF()
-
-  # Make the generated dummy source file depended on all static input
-  # libs. If input lib changes,the source file is touched
-  # which causes the desired effect (relink).
-  ADD_CUSTOM_COMMAND( 
-    OUTPUT  ${SOURCE_FILE}
-    COMMAND ${CMAKE_COMMAND}  -E touch ${SOURCE_FILE}
-    DEPENDS ${STATIC_LIBS})
+  
+  FOREACH(CONFIG_TYPE ${CONFIGURATION_TYPES})
+    IF(STATIC_LIBS_${CONFIG_TYPE})
+      LIST(REMOVE_DUPLICATES STATIC_LIBS_${CONFIG_TYPE})
+    ENDIF()
+  ENDFOREACH()
 
   IF(MSVC)
-    # To merge libs, just pass them to lib.exe command line.
-    #set each config flags
-    IF(CMAKE_CONFIGURATION_TYPES)
-      FOREACH(CONFIG_TYPE ${CMAKE_CONFIGURATION_TYPES})
-        SET(LINKER_EXTRA_FLAGS_${CONFIG_TYPE} "")
-
-        FOREACH(LIB ${STATIC_LIBS})
-          SET(LINKER_EXTRA_FLAGS_${CONFIG_TYPE} "${LINKER_EXTRA_FLAGS_${CONFIG_TYPE}} ${LIB}")
-        ENDFOREACH()
-
-        SET_TARGET_PROPERTIES(${TARGET} PROPERTIES STATIC_LIBRARY_FLAGS_${CONFIG_TYPE}
-          "${LINKER_EXTRA_FLAGS__${CONFIG_TYPE}}")
+    # To merge libs, just pass them to lib.exe command line, per build type
+    FOREACH(CONFIG_TYPE ${CONFIGURATION_TYPES})
+      message(STATUS "${CONFIG_TYPE}: ${STATIC_LIBS_${CONFIG_TYPE}}")
+      SET(LINKER_EXTRA_FLAGS "")
+      FOREACH(LIB ${STATIC_LIBS_${CONFIG_TYPE}})
+          SET(LINKER_EXTRA_FLAGS "${LINKER_EXTRA_FLAGS} ${LIB}")
       ENDFOREACH()
-    ELSE()
-      SET(LINKER_EXTRA_FLAGS_${CMAKE_BUILD_TYPE} "")
+      message(STATUS "${CONFIG_TYPE}: ${LINKER_EXTRA_FLAGS}")
+      SET_TARGET_PROPERTIES(${TARGET} PROPERTIES STATIC_LIBRARY_FLAGS_${CONFIG_TYPE} "${LINKER_EXTRA_FLAGS}")
 
-      FOREACH(LIB ${STATIC_LIBS})
-        SET(LINKER_EXTRA_FLAGS_${CMAKE_BUILD_TYPE} "${LINKER_EXTRA_FLAGS_${CMAKE_BUILD_TYPE}} ${LIB}")
-      ENDFOREACH()
-
-      SET_TARGET_PROPERTIES(${TARGET} PROPERTIES STATIC_LIBRARY_FLAGS_${CMAKE_BUILD_TYPE}
-        "${LINKER_EXTRA_FLAGS__${CMAKE_BUILD_TYPE}}")
-    ENDIF()
+    ENDFOREACH()
   ELSE()
-    GET_TARGET_PROPERTY(TARGET_LOCATION ${TARGET} LOCATION_${CMAKE_BUILD_TYPE})  
-    IF(APPLE)
-      # Use OSX's libtool to merge archives (ihandles universal 
-      # binaries properly)
-      ADD_CUSTOM_COMMAND(TARGET ${TARGET} POST_BUILD
-        COMMAND rm ${TARGET_LOCATION}
-        COMMAND /usr/bin/libtool -static -o ${TARGET_LOCATION} 
-        ${STATIC_LIBS}
-      )  
-    ELSE()
-      # Generic Unix or MinGW. In post-build step, call
-      # script, that extracts objects from archives with "ar x" 
-      # and repacks them with "ar r"
-      SET(TARGET ${TARGET})
-      CONFIGURE_FILE(
-        ${MYSQL_CMAKE_SCRIPT_DIR}/merge_archives_unix.cmake.in
-        ${CMAKE_CURRENT_BINARY_DIR}/merge_archives_${TARGET}.cmake 
-        @ONLY
-      )
-      ADD_CUSTOM_COMMAND(TARGET ${TARGET} POST_BUILD
-        COMMAND rm ${TARGET_LOCATION}
-        COMMAND ${CMAKE_COMMAND} -P 
-        ${CMAKE_CURRENT_BINARY_DIR}/merge_archives_${TARGET}.cmake
-      )
-    ENDIF()
+    FOREACH(CONFIG_TYPE ${CONFIGURATION_TYPES})
+      GET_TARGET_PROPERTY(TARGET_LOCATION ${TARGET} LOCATION_${CONFIG_TYPE}) 
+      SET(STATIC_LIBS ${STATIC_LIBS_${CONFIG_TYPE}})
+      
+      IF(APPLE)
+        # Use OSX's libtool to merge archives (ihandles universal 
+        # binaries properly)
+        ADD_CUSTOM_COMMAND(TARGET ${TARGET} POST_BUILD
+          COMMAND rm ${TARGET_LOCATION}
+          COMMAND /usr/bin/libtool -static -o ${TARGET_LOCATION} 
+          ${STATIC_LIBS_${CONFIG_TYPE}}
+        )  
+      ELSE()
+        # Generic Unix or MinGW. In post-build step, call
+        # script, that extracts objects from archives with "ar x" 
+        # and repacks them with "ar r"
+        SET(TARGET ${TARGET})
+        CONFIGURE_FILE(
+          ${MYSQL_CMAKE_SCRIPT_DIR}/merge_archives_unix.cmake.in
+          ${CMAKE_CURRENT_BINARY_DIR}/merge_archives_${TARGET}.cmake 
+          @ONLY
+        )
+        ADD_CUSTOM_COMMAND(TARGET ${TARGET} POST_BUILD
+          COMMAND rm ${TARGET_LOCATION}
+          COMMAND ${CMAKE_COMMAND} -P 
+          ${CMAKE_CURRENT_BINARY_DIR}/merge_archives_${TARGET}.cmake
+        )
+      ENDIF()
+    ENDFOREACH()
   ENDIF()
 ENDMACRO()
 
